c6602691b8810a085b29778942700e7fbd657436
Nick12345-code <astonishedchocolate@gmail.com>
2021-07-28 13:37:33 +1000

    audio manager + detection manager setup

---
+++ Assets/Nick'sStuff/Scripts/Audio.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Audio : MonoBehaviour
+{
+    public AudioSource master;
+    // sound effects here e.g. public AudioClip doorCreak
+}
