4f7f40e0c17e0f9805f2cd06033d8c200bcf3548
XavierVibes <xavier_rate@outlook.com>
2021-08-04 13:03:50 +1000

    Creating the adaptly called MotherVisionTM (all rights reserved) basically when the mother sees the player its immediate gameover still need to add logic that does that! For now I'm tuning the statemachines

---
+++ Assets/Xavier's Stuff/Scipts/MotherState.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MotherIntelligence
+{
+    public enum States
+    {
+        SearchingChild,
+        FoundChild,
+        GroundChild
+    }
+    
+    
+    public class MotherState : MonoBehaviour
+    {
+        
+
+    }
+}
