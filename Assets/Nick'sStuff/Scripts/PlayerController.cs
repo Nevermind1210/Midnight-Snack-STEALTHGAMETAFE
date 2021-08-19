33ef2b3a88afb91f0725811afe75c17e3befc127
Nick12345-code <astonishedchocolate@gmail.com>
2021-08-19 11:26:47 +1000

    Navmesh Based Player Controller

--- Assets/Nick'sStuff/Scripts/PlayerController.cs
+++ Assets/Nick'sStuff/Scripts/PlayerController.cs
@@ -1,54 +1,46 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Transform pointer;
-    [SerializeField] private float speed;
-    [SerializeField] private float reach;
-    private Rigidbody rb;
-    private Vector2 mousePosition;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    [SerializeField] private NavMeshAgent player;       // the player agent
+    [SerializeField] private Transform pointer;         // interaction raycast is shot from this
+    [SerializeField] private float reach;               // how far player can reach to interact
 
     private void Update()
     {
-        Interact();
-    }
-
-    void FixedUpdate()
-    {
         Movement();
-        Rotation();
+        Interact();
     }
 
     private void Movement()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        rb.velocity = new Vector3(horizontal * speed, rb.velocity.y, vertical * speed);
-    }
-
-    private void Rotation()
-    {
-        // used to store information on what raycast hits
-        RaycastHit hit;
-        // raycast is shot from mouse position
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        // if the raycast hits something (which it always should)
-        if (Physics.Raycast(ray, out hit))
+        // if the left mouse button is held down
+        if (Input.GetMouseButton(0))
         {
-            if (hit.collider.CompareTag("Player"))
+            // ray is shot from mouse position
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // used to store information on what raycast hit
+            RaycastHit hit;
+
+            // raycast is shot our from click position
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
-                // this stops some jittering
-            }
-            else
-            {
-                // the player rotates towards that position
-                transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+                // creates a path that can be calculated
+                NavMeshPath path = new NavMeshPath();
+                // calculates the path of the most recent click
+                player.CalculatePath(hit.point, path);
+                // if path is blocked
+                if (path.status == NavMeshPathStatus.PathPartial)
+                {
+                    // player doesn't move if they can't reach the destination
+                }
+                // else if the path is clear
+                else
+                {
+                    // player moves towards the hit position
+                    player.SetDestination(hit.point);
+                }             
             }
         }
     }
@@ -56,7 +48,7 @@ public class PlayerController : MonoBehaviour
     private void Interact()
     {
         // if 'E' button is pressed
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             // used to store information on what raycast hits
             RaycastHit hit;
@@ -65,12 +57,12 @@ public class PlayerController : MonoBehaviour
             if (Physics.Raycast(pointer.position, pointer.transform.forward, out hit, reach))
             {
                 // if the raycast hits a collider tagged 'Fridge'
-                if (hit.collider.CompareTag("Fridge"))
+                if (hit.collider.name == "Fridge")
                 {
                     // get food from the fridge
                     print("you are interacting with the fridge!");
                 }
-                else if (hit.collider.CompareTag("Door"))
+                else if (hit.collider.name == "Door")
                 {
                     // open the door
                     print("you are interacting with the door!");
