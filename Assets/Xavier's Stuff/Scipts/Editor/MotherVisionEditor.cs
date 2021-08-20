b38cd5c0047253573470ba812f392afcbfe027db
Xavier Arias <xavier_rate@outlook.com>
2021-08-21 01:07:40 +1000

    Vision works! I am going to try the pathing soon, I might have a prototype of hearing soon, I'll keep tinkering heres this though!

---
+++ Assets/Xavier's Stuff/Scipts/Editor/MotherVisionEditor.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace MotherIntelligence.Editor
+{
+    [CustomEditor(typeof(MotherVision))]
+    public class MotherVisionEditor : UnityEditor.Editor
+    {
+        private void OnSceneGUI()
+        {
+            MotherVision fov = (MotherVision) target;
+            Handles.color = Color.white;
+            Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.radius);
+
+            Vector3 viewAngle01 = DirectionFromAngle(fov.transform.eulerAngles.y, -fov.angle / 2);
+            Vector3 viewAngle02 = DirectionFromAngle(fov.transform.eulerAngles.y, +fov.angle / 2);
+
+            Handles.color = Color.yellow;
+            Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle01 * fov.radius);
+            Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle02 * fov.radius);
+
+            if (fov.canSeePlayer)
+            {
+                Handles.color = Color.green;
+                Handles.DrawLine(fov.transform.position, fov.playerRef.transform.position);
+            }
+        }
+
+        private Vector3 DirectionFromAngle(float eulerY, float anglesInDegrees)
+        {
+            anglesInDegrees += eulerY;
+
+            return new Vector3(Mathf.Sin(anglesInDegrees * Mathf.Deg2Rad), 0,
+                Mathf.Cos(anglesInDegrees * Mathf.Deg2Rad));
+        }
+    }
+}
--- Assets/Xavier's Stuff/Scipts/MotherWandering.cs
+++ Assets/Xavier's Stuff/Scipts/MotherWandering.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MotherWandering : MonoBehaviour
 {
+    [SerializeField] float wanderRadius;
+    [SerializeField] float wanderTimer;
+
+    private Transform target;
+    private NavMeshAgent agent;
+    private float timer;
+    private Animator _anim;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        _anim = GetComponent<Animator>();
+        target = transform;
+        timer = wanderTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            if (!agent.pathPending && agent.remainingDistance < 0.1f)
+            {
+                Vector3 newPos = RandomNavSphere(target.position, wanderRadius, -1);
+                agent.SetDestination(newPos);
+                timer = 0;
+            }
+
+            //_anim.SetTrigger("");
+        }
+        else if (target == null)
+        {
+            //anim.SetTrigger("");
+        }
+    }
+    
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
     }
 }
